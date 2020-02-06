b820ead374ae8b356d68b706423030bae9fb4cb4
UO263909 <UO263909@uniovi.es>
2020-02-06 20:08:55 +0100

    Creación inicial del proyecto sdi1920-606-lab-spring

---
+++ sdi1920-606-lab-spring/src/main/java/com/uniovi/Sdi1920606LabSpringApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi1920606LabSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdi1920606LabSpringApplication.class, args);
+	}
+
+}
